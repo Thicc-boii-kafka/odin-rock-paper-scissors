eab13241556b62aa8d6d7a4b9c77caea6192ede8
Thicc-boii-kafka <awideajdid@gmail.com>
2022-06-22 00:20:45 +0100

    Finished the project

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 function computerPlay() {
-    let computerSelection;
-    let playerSelection;
+
+
     let string = ['rock', 'paper', 'scissors'];
 
     function getRandomIntInclusive(min, max) {
@@ -12,16 +12,43 @@ function computerPlay() {
     return string[result];
 
 }
-let compturn = computerPlay();
+let computerScore = 0;
+let playerScore = 0;
+let playerSelection;
 
-console.log(compturn);
+
+
+const computerSelection = computerPlay();
 
 function playRound(playerSelection, computerSelection) {
-
-    playerSelection = playerSelection().toLowerCase();
+    playerSelection = prompt("please choose between rock, paper, scissors");
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection == computerSelection) {
+        playerScore += 1;
+        computerScore += 1;
+        return ("It's a tie, you both picked: " + playerSelection + " and the score is : You : " + playerScore + "  Computer : " + computerScore)
+    } else if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper")) {
+        playerScore += 1;
+        return ("You win! " + playerSelection + " beats " + computerSelection + ". The score is: You : " + playerScore + " Computer : " + computerScore)
+    } else {
+        computerScore += 1;
+        return ("you lose! " + computerSelection + " beats " + playerSelection + ". The score is: You : " + playerScore + " Computer : " + computerScore)
+    }
 
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    if (playerScore === computerScore) {
+        return "This game is a tie."
+    } else if (playerScore < computerScore) {
+        return "you lose the game."
+    } else {
+        return "You win the game."
+    }
+
+}
+console.log(game());
